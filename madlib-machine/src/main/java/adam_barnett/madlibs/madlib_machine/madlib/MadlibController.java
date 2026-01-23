c0bdf95c940f84708be946348f136451c5ac7d33
adam-lev-barnett <adam.lev.barnett@gmail.com>
2026-01-23 13:55:21 -0600

    Create unit tests for MadlibFiller

--- madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/madlib/MadlibController.java
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/madlib/MadlibController.java
@@ -1,11 +1,9 @@
 package adam_barnett.madlibs.madlib_machine.madlib;
 
 import adam_barnett.madlibs.madlib_machine.utility.exceptions.InvalidPartOfSpeechException;
-import adam_barnett.madlibs.madlib_machine.utility.exceptions.TextNotProcessedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +13,13 @@ public class MadlibController {
     private final MadlibService madlibService;
 
     @PostMapping("/madlibify")
-    public BlankMadlibResponse madlibifyText(@RequestBody BlankMadlibRequest sourceTextAndSkipper) throws TextNotProcessedException, InvalidPartOfSpeechException, IOException {
+    public BlankMadlibResponse madlibifyText(@RequestBody BlankMadlibRequest sourceTextAndSkipper) throws InvalidPartOfSpeechException {
         return madlibService.generateBlankMadlib(sourceTextAndSkipper.sourceText(), sourceTextAndSkipper.skipper());
     }
 
-//    @PostMapping("/fillMadlib")
-//    public FilledMadlibResponse fillInMadlib(@RequestBody FillMadlibRequest blankTextAndWordList) throws IOException {
-//        return madlibService.fillInMadlib(blankTextAndWordList.blankedText(), blankTextAndWordList.partsOfSpeech());
-//    }
+    @PostMapping("/fillMadlib")
+    public FilledMadlibResponse fillInMadlib(@RequestBody FillMadlibRequest blankTextAndWordList) {
+        return madlibService.fillInMadlib(blankTextAndWordList.blankedText(), blankTextAndWordList.replacementWords());
+    }
 
 }
