62f43e2df7aa33a3ccaa7ab64d67eb569a0284b9
adam-lev-barnett <adam.lev.barnett@gmail.com>
2026-01-26 16:59:02 -0600

    Update CORS

--- madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/CorsConfig.java
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/CorsConfig.java
@@ -1,24 +1,20 @@
 package adam_barnett.madlibs.madlib_machine;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://madlib-frontend-deploy.vercel.app") // Put your Vercel URL here
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://madlib-frontend-deploy-97r19iulr.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
