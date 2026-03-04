5c6dd246d01f72095e91db120ca4434a75af7d26
Adam Barnett <adam.lev.barnett@gmail.com>
2026-03-04 15:28:01 -0600

    Create OpenApiConfig file for Swagger authentication with Google OAuth 2.0

---
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/security/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package adam_barnett.madlibs.madlib_machine.security.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Bearer")
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
