1767c797e29c4c2bfc5fdddede32b6a1e5a3a0bd
Adam Barnett <adam.lev.barnett@gmail.com>
2026-03-05 10:14:05 -0600

    Update CorsConfig to allow for local Google account connectivity

--- madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/security/config/CorsConfig.java
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/security/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.vercel.app")
+                .allowedOriginPatterns("https://*.vercel.app", "http://localhost:*")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*");
     }
