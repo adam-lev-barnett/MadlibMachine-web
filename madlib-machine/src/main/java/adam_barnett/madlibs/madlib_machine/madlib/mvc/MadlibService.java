0518f5cf372b778ac8e4d436d8bf902cb0b133da
Adam Barnett <adam.lev.barnett@gmail.com>
2026-03-06 11:36:08 -0600

    Database bugfix

--- madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/madlib/mvc/MadlibService.java
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/madlib/mvc/MadlibService.java
@@ -54,11 +54,6 @@ public class MadlibService {
         if (auth != null && auth.getPrincipal() instanceof User user) {
             madlibRepository.save(new Madlib(completedMadlib, user));
         }
-        //! Hard coding admin ID to use for public Madlib cache
-        else {
-            Optional<User> publicUser = userRepository.findByUserId("96294076-3f19-4998-971e-e193eb9266a3");
-            madlibRepository.save(new Madlib(completedMadlib, publicUser.get()));
-        }
 
         return new FilledMadlibResponse(completedMadlib);
     }
