3046883ee9d5703354635f214d7af740773fded4
Adam Barnett <adam.lev.barnett@gmail.com>
2026-04-30 12:32:39 -0500

    Update cors config and env var to allow for new domain redirect (madlib-machine.app)

--- madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/security/config/CorsConfig.java
+++ madlib-machine/src/main/java/adam_barnett/madlibs/madlib_machine/security/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.vercel.app", "http://localhost:*")
+                .allowedOriginPatterns("https://*.vercel.app", "http://localhost:*", "https://madlib-machine.app", "https://www.madlib-machine.app")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*");
     }
